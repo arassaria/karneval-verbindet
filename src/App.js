2b5c068d88aaece955bbbda8add7db3ce8b85fdd
domko95 <dominique.kolbe1995@googlemail.com>
2021-02-04 11:09:53 +0100

    :sparkles: Add Twitchplayer and Chat to Page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <iframe
+        src="https://player.twitch.tv/?channel=zock4fun92&parent=localhost"
+        frameborder="0"
+        allowfullscreen="true"
+        scrolling="no"
+        height="378"
+        width="620"
+      ></iframe>
+      <iframe
+        id="chat_embed"
+        src="https://www.twitch.tv/embed/zock4fun92/chat?parent=localhost"
+        height="500"
+        width="350"
+      ></iframe>
     </div>
   );
 }
