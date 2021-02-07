41566c41ea642326e31305afc9bbe64fac198f68
domko95 <dominique.kolbe1995@googlemail.com>
2021-02-07 17:28:29 +0100

    :sparkles: Add iframe

--- src/App.js
+++ src/App.js
@@ -1,16 +1,29 @@
 import "./App.css";
-import flyer from "./assets/karneval-verbindet.png";
 
 function App() {
   return (
     <div className="App">
-      <img
-        className="Flyer"
-        src={flyer}
-        alt="Karneval verbindet"
-        width="1920"
-        height="1080"
-      />
+      <iframe
+        title="Youtube"
+        width="960"
+        height="540"
+        src="https://youtube.com/embed/RuA-79Emp1g"
+        frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowfullscreen
+      ></iframe>
+      <iframe
+        title="LiveChat"
+        width="300"
+        height="540"
+        src="https://www.youtube.com/live_chat?v=RuA-79Emp1g&amp;embed_domain=localhost"
+      ></iframe>
+      <form className="Form">
+        <input type="Text" placeholder="Dein Name" />
+        <input type="Text" placeholder="Titel des Songs" />
+        <input type="Text" placeholder="Interpret" />
+        <button type="submit">Abschicken</button>
+      </form>
     </div>
   );
 }
