fe3528156163842bdc3a1921834d6359e1e76f1e
domko95 <dominique.kolbe1995@googlemail.com>
2021-02-10 01:11:06 +0100

    :sparkles: Add responsive background image

--- src/App.js
+++ src/App.js
@@ -2,17 +2,7 @@ import "./App.css";
 import flyer from "./assets/karneval-verbindet.png";
 
 function App() {
-  return (
-    <div className="App">
-      <img
-        className="Flyer"
-        src={flyer}
-        alt="Karneval verbindet"
-        width="1920"
-        height="1080"
-      />
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
