a10b06de82068c7c4fdf8602fe59746ff02cfd37
domko95 <dominique.kolbe1995@googlemail.com>
2022-02-14 13:26:32 +0100

    :sparkles: update page

--- src/App.js
+++ src/App.js
@@ -1,55 +1,7 @@
 import "./App.css";
-import hachenburger from "./assets/hachenburger.png";
-import stadtLogo from "./assets/stadt logo.png";
-import esLogo from "./assets/logo-eventservice.png";
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Karneval Verbindet</h1>
-      <div className="ContentWrapper">
-        {/* <div className="Stream">
-          <iframe
-            src="https://kolbe-live.de/embed/video"
-            title="Owncast"
-            height="350px"
-            width="550px"
-            referrerpolicy="origin"
-            scrolling="no"
-            allowFullScreen
-          ></iframe>
-          <iframe
-            src="https://kolbe-live.de/embed/chat/readwrite"
-            title="Chat"
-            height="350px"
-            width="300px"
-            referrerpolicy="origin"
-            scrolling="no"
-          ></iframe>
-        </div> */}
-        <div className="Footer">
-          <h3>Unsere Sponsoren</h3>
-          <div className="LogoContainer">
-            <img src={hachenburger} alt="Hachenburger" />
-            <img src={stadtLogo} alt="Karneval Montabaur" />
-            <img
-              src={esLogo}
-              alt="Eventservice Mario Bachmeier"
-              className="ESLogo"
-            />
-          </div>
-          <div className="LinkContainer">
-            <a href="https://www.eventservice-mb.de/rechtliches/impressum/">
-              Impressum
-            </a>
-            <a href="https://www.eventservice-mb.de/rechtliches/datenschutz/">
-              Datenschutz
-            </a>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
